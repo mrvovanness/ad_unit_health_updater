d2eb6be10e9fd92ba62e40e47fe5bc3178410421
mrvovanness <mrvovanness@gmail.com>
2017-01-24 11:04:41 +0500

    Progress

--- main.go
+++ main.go
@@ -2,22 +2,47 @@ package main
 
 import (
   "fmt"
-  "time"
+  //"time"
   "database/sql"
   _ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-  start_time := time.Now()
-  fmt.Printf("The time now is %s\n", start_time)
-  db, err := sql.Open("mysql", "dev:1111@/a-ads_development")
-  fmt.Printf("db is %s\n, error is %s\n", db, err)
+  db, _ := sql.Open("mysql", "root:root@/a-ads_development")
   defer db.Close()
 
-  err2 := db.Ping()
-  if err2 != nil {
-    fmt.Printf("err is %s\n", err2)
-  } else {
-    fmt.Println("Connection working")
+  err_conn := db.Ping()
+  if err_conn != nil {
+    fmt.Printf("err is %s\n", err_conn)
+    return
   }
+  rows, err := db.Query(query_pages("ranks_updated_at"))
+  if err != nil {
+    fmt.Printf("err is %s\n", err)
+    return
+  }
+  defer rows.Close()
+
+  var (
+    id int
+    ranks_updated_at sql.NullString
+    ranks_string string
+  )
+
+  for rows.Next() {
+    err := rows.Scan(&id, &ranks_updated_at)
+    if err != nil {
+      fmt.Printf("err is %s\n", err)
+    }
+    if ranks_updated_at.Valid {
+      ranks_string = ranks_updated_at.String
+      fmt.Println(id, ranks_string)
+    } else {
+      fmt.Printf("%s, %#v \n", id, ranks_updated_at)
+    }
+  }
+}
+
+func query_pages(order_by string) string {
+  return fmt.Sprintf("SELECT id, ranks_updated_at FROM pages ORDER BY %s ASC", order_by)
 }
